82b8502a469767c9e97c9ef4c70503a9e6c03c54
Akash Kumar <kr.akash149@gmail.com>
2024-03-24 18:33:43 +0530

    Added todo creation

--- src/main/java/com/example/ToDoAPI/ToDoController.java
+++ src/main/java/com/example/ToDoAPI/ToDoController.java
@@ -1,10 +1,11 @@
 package com.example.ToDoAPI;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class ToDoController {
 
     private static List<Todo> toDoList;
@@ -13,6 +14,17 @@ public class ToDoController {
         toDoList = new ArrayList<>();
         toDoList.add(new Todo(1, false, "ToDo 1", 1));
         toDoList.add(new Todo(2, true, "ToDo 2", 2));
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> getToDos() {
+        return toDoList;
+    }
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo) {
+        toDoList.add(newTodo);
+        return newTodo;
     }
 
 }
